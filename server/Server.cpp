b46417d5d8ce918f9cd3b7202cc8c30fc22a6382
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2020-09-14 11:48:21 +0700

    Init winsock.

---
+++ server/Server.cpp
@@ -0,0 +1,42 @@
+/*
+The Winsock2.h header file internally includes core elements from the Windows.h
+header file, so there is not  usually an #include line for the Windows.h header
+file in Winsock applications. If an #include line is needed for the Windows.h
+header file, this should be preceded with the #define WIN32_LEAN_AND_MEAN
+macro. For historical reasons, the Windows.h header defaults to including the
+Winsock.h header file for Windows Sockets 1.1. The declarations in the
+Winsock.h header file will conflict with the declarations in the Winsock2.h
+header file required by Windows Sockets 2.0. The WIN32_LEAN_AND_MEAN macro
+prevents the Winsock.h from being included by the Windows.h header.
+*/
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+The #pragma comment indicates to the linker that the Ws2_32.lib file is needed.
+*/
+#pragma comment(lib, "Ws2_32.lib")
+
+void initWinsock() {
+	WSADATA wsaData;
+
+	// Initialize Winsock
+	int initStatus = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (initStatus != 0) {
+		printf("WSAStartup failed: %d\n", initStatus);
+		exit(1);
+	}
+	printf("Winsock initialized.\n");
+}
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
+
